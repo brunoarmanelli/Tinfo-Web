f47248b3ff2bdc3c5d8ea8c81a8d76bb94536723
Bruno Marini <bruno.marini1@gmail.com>
2018-12-05 22:16:55 -0200

    Pre-presentation [^]

--- assets/js/ic.js
+++ assets/js/ic.js
@@ -8,59 +8,119 @@ var tipos = [{
 }]
 
 // Static fornecedores
-var fornecedores = ["Apple", "Asus", "Hewlett-Packard", "Lenovo"]
+// var fornecedores = ["Apple", "Asus", "Hewlett-Packard", "Lenovo"]
+var fornecedores = [];
 
 // Static responsáveis
 var responsaveis = ["Alan Turing", "Linus Torvalds", "Bill Gates", "Steve Jobs"]
 
 // --- Document ready ---
 $(function () {
+  $('#tabelaIC').DataTable({
+    "initComplete": function () { $('[data-toggle="tooltip"]').tooltip() },
+    responsive: true,
+    "language": {
+    "url": "https://cdn.datatables.net/plug-ins/1.10.19/i18n/Portuguese-Brasil.json"
+    },
+    "processing" : true,
+    "ajax" : {
+        "url" : `${HOST}/getICs`,
+        dataSrc : ''
+    },
+    "columns" : [ {
+        "data" : "id"
+    },{
+        "data" : "nome"
+    }, {
+        "data" : "subtipo"
+    }, {
+        "data" : "status"
+    }, {
+      "data"   : null
+    }],
+    "columnDefs": [ {
+        "targets": -1,
+        "data": null,
+        "defaultContent": html
+    } ]
+  });
+
   // --- Inputs ---
   var inputTipo = $("#inputTipo");
   var inputSubtipo = $("#inputSubtipo");
   var inputFornecedor = $("#inputFornecedor");
-  var inputResponsavel = $("#modalUpdate #inputResponsavel");
+  var inputResponsavel = $(".specificity #inputResponsavel");
 
   function loadSelectData () {
     // --- Data loads ---
     // Tipos
     if (tipos && tipos.length > 0) {
-      for (var i = 0; i < tipos.length; i++) {
-        loadSelect(inputTipo, [tipos[i].nome]);
-        if (tipos[i].subtipos && tipos[i].subtipos.length > 0)
-          loadSelect(inputSubtipo, tipos[i].subtipos);
-        else
-          disableSelect(inputSuptipo, "subtipo");
-      }
-    }
-    else {
-      disableSelect(inputTipo, "tipo");
-      disableSelect(inputSuptipo, "subtipo");
-    }
+      var aux_tipos = [];
+      for (var i = 0; i < tipos.length; i++)
+        aux_tipos.push(tipos[i].nome);
 
-    // Fornecedores
+      loadSelect(inputTipo, aux_tipos)
+    } else
+      disableSelect(inputTipo, "tipo");
+
+
     if (fornecedores && fornecedores.length > 0)
-      loadSelect(inputFornecedor, fornecedores);
-    else {
+      loadSelect(inputFornecedor, fornecedores)
+    else
       disableSelect(inputFornecedor, "fornecedor");
-    }
   }
 
-  $(".btnAdd").on("click", function () {
+  getFornecedores(function (data) {
+    for (var i = 0; i < data.length; i++)
+      fornecedores.push(data[i].nomeFantasia)
+
     loadSelectData();
+  });
+
+  $(inputTipo).on("change", function () {
+    // --- Load Subtipo ---
+    inputSubtipo.html("");
+
+    var opc = $(this).val();
+
+    if (opc.length > 0)
+      if (tipos[opc].subtipos && tipos[opc].subtipos.length > 0)
+        loadSelect(inputSubtipo, tipos[opc].subtipos);
+      else
+        disableSelect(inputSubtipo, "subtipo");
+    else {
+      inputSubtipo.attr("disabled", true)
+      inputSubtipo.html("<option selected>-</option>");
+    }
   })
 
-  $(".btnUpdate").on("click", function () {
+  $(inputSubtipo).on("change", function () {
+    // --- Specificity ---
+    var opc = $(this).val();
+
+    if (opc.length > 0 && opc == 0) {
+      $(".specificity").removeClass("d-none");
+      var nomes = [];
+      getUsuarios(function (data) {
+        for (var i = 0; i < data.length; i++)
+          nomes.push(data[i].nome)
+
+        loadSelect(inputResponsavel, nomes)
+      })
+    } else
+      $(".specificity").addClass("d-none");
   })
+
+  $(".btnAdd").on("click", function () {
+
+  })
+
+  $(".btnUpdate").on("click", function () {})
 
   // --- Table ---
   var table = $("#tabelaIC");
   var modal = $("#modalIC");
   var modal_header = $("#modalIC .modal-header")
-
-  // $("#tabelaIC tbody").on("click", ".btnView", function () {
-  //
-  // })
 
   $("#tabelaIC tbody").on("click", ".btnUpdate", function () {
     // Fornecedores
@@ -69,5 +129,9 @@ $(function () {
     else {
       disableSelect(inputResponsavel, "fornecedor");
     }
+  })
+
+  $("#tabelaIC tbody").on("click", ".btnView", function () {
+    getICs(function (data) { });
   })
 })
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,142 @@
+// --- HOST const ---
+const HOST = "http://192.168.43.169:8080/Tinfo";
+
+// --- Request ---
+function requestError (error, name) {
+  if (name)
+    console.error(`Erro ao fazer requisição para buscar ${name}: `, error);
+}
+
+// --- Getters ---
+function getICs (success, error) {
+  $.ajax({
+    url: `${HOST}/getICs`,
+    type: "GET",
+    dataType: 'json',
+    success: function (data) {
+      success(data)
+    },
+    error: function (e) {
+      if (error && typeof error === "function")
+        error(e);
+      else
+        requestError(e, "ICs");
+    }
+  });
+}
+
+function getSubtipo (subtipo, success, error) {
+  var url = HOST;
+  switch (subtipo) {
+    case "computador":
+      url += "/getComputadores"
+      break;
+
+    default:
+    url += "/getFiltroIC?subtipo=" + subtipo
+  }
+
+  $.ajax({
+    url: url,
+    type: "GET",
+    dataType: 'json',
+    success: function (data) {
+      success(data)
+    },
+    error: function (e) {
+      if (error && typeof error === "function")
+        error(e);
+      else
+        requestError(e, "subtipo");
+    }
+  });
+}
+
+function getComputadores (success, error) {
+  $.ajax({
+    url: `${HOST}/getComputadores`,
+    type: "GET",
+    dataType: 'json',
+    success: function (data) {
+      success(data)
+    },
+    error: function (e) {
+      if (error && typeof error === "function")
+        error(e);
+      else
+        requestError(e, "subtipo");
+    }
+  });
+}
+
+function getDepartamentos (success, error) {
+  $.ajax({
+    url: `${HOST}/getDepartamentos`,
+    type: "GET",
+    dataType: 'json',
+    success: function (data) {
+      success(data)
+    },
+    error: function (e) {
+      if (error && typeof error === "function")
+        error(e);
+      else
+        requestError(e, "subtipo");
+    }
+  });
+}
+
+function getFornecedores (success, error) {
+  $.ajax({
+    url: `${HOST}/getFornecedores`,
+    type: "GET",
+    dataType: 'json',
+    success: function (data) {
+      success(data)
+    },
+    error: function (e) {
+      if (error && typeof error === "function")
+        error(e);
+      else
+        requestError(e, "subtipo");
+    }
+  });
+}
+
+function getUsuarios (success, error) {
+  $.ajax({
+    url: `${HOST}/getUsuarios`,
+    type: "GET",
+    dataType: 'json',
+    success: function (data) {
+      success(data)
+    },
+    error: function (e) {
+      if (error && typeof error === "function")
+        error(e);
+      else
+        requestError(e, "subtipo");
+    }
+  });
+}
+
+function getFabricantes (success, error) {
+  $.ajax({
+    url: `${HOST}/getFabricantes`,
+    type: "GET",
+    dataType: 'json',
+    success: function (data) {
+      success(data)
+    },
+    error: function (e) {
+      if (error && typeof error === "function")
+        error(e);
+      else
+        requestError(e, "subtipo");
+    }
+  });
+}
+
 // --- Document ready ---
 $(function () {
   $('[data-toggle="tooltip"]').tooltip()
@@ -12,9 +151,13 @@ function disableSelect ($element, title) {
 
 // Load data on a select HTML element
 function loadSelect ($element, data, firstDash = true) {
-  if (firstDash && $element.children().length == 0)
-    $element.append("<option selected>-</option>");
+  if (data.length > 0){
+    $element.attr("disabled", false);
 
-  for (var i = 0; i < data.length; i++)
-    $element.append(`<option value="${i}">${data[i]}</option>`);
+    if (firstDash && $element.children().length == 0)
+      $element.append(`<option value="" selected>-</option>`);
+
+    for (var i = 0; i < data.length; i++)
+      $element.append(`<option value="${i}">${data[i]}</option>`);
+  }
 }
